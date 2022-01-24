fbdfd4435e787f08ead929f586d4b1bf41c5f691
VincenzoRea <91210630+VincenzoRea@users.noreply.github.com>
2022-01-24 18:59:17 -0500

    myinfo version 2

--- myinfo.py
+++ myinfo.py
@@ -2,4 +2,7 @@
 #Vincenzo Rea
 #MyInfo
 print ('My name is Vincenzo Rea')
-print ('I am a student')
+print ('I live at 1000 Main Street, Manalapan, NJ, 07726')
+print ('My telephone number is 732-900-1000')
+print ('I am a Computer Science major')
+
