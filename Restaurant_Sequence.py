a8ac024a408ae2736088347c1f7be321539394e8
VincenzoRea <91210630+VincenzoRea@users.noreply.github.com>
2022-02-09 17:33:11 -0500

    Add files via upload

---
+++ Restaurant_Sequence.py
@@ -0,0 +1,71 @@
+#CS175
+#VincenzoRea
+#Restaurant_sequence
+
+
+
+
+
+vegetarian = input('Is anyone in your party a vegetarian?')
+
+if vegetarian == 'yes' or vegetarian == 'no':
+    vegan = input('Is anyone in your party vegan?')
+
+if vegan == 'yes' or vegan == 'no':
+    gluten_free = input ('Is anyone in your party gluten free?')
+
+print ('Here are your restaurant choices:')
+
+if gluten_free == 'yes' and vegan == 'yes' and vegetarian == 'yes':
+    print ('Corner Cafe')
+    print ("The Chef's Kithcen")
+
+if gluten_free == 'no' and vegan == 'no' and vegetarian == 'no':
+    print ('Corner Cafe')
+    print ("The Chef's Kithcen")
+    print ("Joe's Gormet Burgers")
+    print ('Main Street Pizza Company')
+    print ("Mama's Fine Italian")
+
+if gluten_free == 'yes' and vegan == 'no' and vegetarian == 'no':
+    print ('Main Street Pizza Company')
+    print ("The Chef's Kithcen")
+    print ('Corner Cafe')
+
+if gluten_free == 'no' and vegan == 'yes' and vegetarian == 'no':
+     print ('Corner Cafe')
+     print ("The Chef's Kithcen")
+
+if gluten_free == 'no' and vegan == 'no' and vegetarian == 'yes':
+    print ('Corner Cafe')
+    print ("The Chef's Kithcen")
+    print ('Main Street Pizza Company')
+    print ("Mama's Fine Italian")
+
+if gluten_free == 'yes' and vegan == 'yes' and vegetarian == 'no':
+    print ('Corner Cafe')
+    print ("The Chef's Kithcen")
+
+if gluten_free == 'no' and vegan == 'yes' and vegetarian == 'yes':
+    print ('Corner Cafe')
+    print ("The Chef's Kithcen")
+
+if gluten_free == 'yes' and vegan == 'no' and vegetarian == 'yes':
+    print ('Main Street Pizza Company')
+    print ("The Chef's Kithcen")
+    print ('Corner Cafe')
+    
+
+
+    
+
+    
+    
+
+#print(input('Is anyone in your party vegan?'))
+
+#print(input('Is anyone in your party gluten free?'))
+
+
+
+
